7b2b353d197cef824d4620ec55f0d265f0828a33
Shravan Jambukesan <shravan.j97@gmail.com>
2020-08-19 00:03:22 -0500

    Initial commit

---
+++ PMStockSplitService/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+/*
+ * PMStockSplitService - Start of Day Stock Split Service Module for Pseudo Markets
+ * Author: Shravan Jambukesan <shravan@shravanj.com>
+ * Date: 8/18/20
+ */
+
+namespace PMStockSplitService
+{
+    public class Program
+    {
+        public static IConfigurationRoot configuration;
+        static void Main(string[] args)
+        {
+            // Setup the Serilog logger
+            string logFileName = "PMStockSplitService-" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(logFileName)
+                .CreateLogger();
+            Log.Information("Starting PMStockSplitService");
+
+            // Perform stock split and get the number of records (positions) affected
+            int recordsAffected = PerformSplits().GetAwaiter().GetResult();
+            Log.Information("Records affected: " + recordsAffected);
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // Inject Serilog
+            serviceCollection.AddSingleton(LoggerFactory.Create(builder =>
+            {
+                builder.AddSerilog(dispose: true);
+            }));
+
+            serviceCollection.AddLogging();
+
+            // Setup our application config
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+
+            serviceCollection.AddTransient<Program>();
+        }
+
+        public static async Task<int> PerformSplits()
+        {
+            int numRecordsAffected = 0;
+            ServiceCollection serviceCollection = new ServiceCollection();
+
+            // Configure the config service so we can fetch the connection string from it
+            ConfigureServices(serviceCollection);
+
+            try
+            {
+                await using (var db = new PseudoMarketsDbContext())
+                {
+                    Log.Information("Connected to database");
+
+                    // Find the splits that are happening today
+                    var splits = await db.StockSplits.Where(x => x.ExDate == DateTime.Today).ToListAsync();
+
+                    foreach (StockSplits stockSplit in splits)
+                    {
+                        Log.Information("Found split for symbol " + stockSplit.Symbol + " for date " + stockSplit.ExDate);
+                        int ratio = stockSplit.Ratio;
+
+                        // Find the affected positions by the symbol
+                        var affectedPositions = await db.Positions.Where(x => x.Symbol == stockSplit.Symbol).ToListAsync();
+                        foreach (Positions position in affectedPositions)
+                        {
+                            // Update the quantity based on the split ratio
+                            position.Quantity = position.Quantity * ratio;
+                            db.Entry(position).State = EntityState.Modified;
+                            numRecordsAffected++;
+                            Log.Information("Updated positions for symbol " + position.Symbol + " with a ratio of " + ratio);
+                        }
+                    }
+
+                    // Save changes back to DB
+                    await db.SaveChangesAsync();
+                }
+
+                return numRecordsAffected;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.ToString());
+                throw;
+            }
+
+        }
+    }
+}
---
+++ PMStockSplitService/PseudoMarketsDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
+
+
+/*
+ * Pseudo Markets Database Context
+ * Author: Shravan Jambukesan <shravan@shravanj.com>
+ * Date: 8/18/20
+ */
+
+
+namespace PMStockSplitService
+{
+    public class PseudoMarketsDbContext : DbContext
+    {
+        public DbSet<Positions> Positions { get; set; }
+        public DbSet<StockSplits> StockSplits { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Grab the connection string from appsettings.json
+            var connectionString = Program.configuration.GetConnectionString("PMDB");
+
+            // Use the SQL Server Entity Framework Core connector
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    // Data model for the Positions table
+    public class Positions
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int OrderId { get; set; }
+        public double Value { get; set; }
+        public string Symbol { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    // Data model for the StockSplits table
+    public class StockSplits
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+        public int Ratio { get; set; }
+        public DateTime ExDate { get; set; }
+    }
+}
